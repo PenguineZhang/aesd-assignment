dbfed4442c42aadb5db2d4b1822487e0a97dbedd
Peng Zhang <pengzhang40@gmail.com>
2023-05-07 21:39:56 -0700

    added comments

--- finder-app/finder.sh
+++ finder-app/finder.sh
@@ -3,10 +3,12 @@
 filesdir=$1
 searchstr=$2
 
+# if any of the two arguments is missing, output error
 if [ $# -lt 2 ]
 then
     echo "Must specify <file path> and <search string>"
     exit 1
+# if the directory doesn't exist, output error
 elif [ -e "filesdir" ]
 then
     echo "file directory does not exist"
@@ -14,5 +16,5 @@ then
 else
     num_file=$(ls -1 $filesdir | wc -l | awk '{print $1}')
     output=$(grep -r $searchstr $filesdir | wc -l | awk '{print $1}')
-    echo "The number of files are $num_file and the number of matching lines are $output"
+    echo "The number of files are $num_file and the number of matching lines are $output."
 fi
